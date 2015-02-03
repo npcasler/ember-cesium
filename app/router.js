b2fb0b5d8ef7e6d5519b0c4ac5155172206d947e
dguaderrama <dguaderrama@email.arizona.edu>
2015-02-03 15:24:37 -0700

    Updated application nav and welcome.hbs

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route("cesium");
   this.route("headlines");
+  this.route("welcome", { path: "/"});
 });
 
 export default Router;
